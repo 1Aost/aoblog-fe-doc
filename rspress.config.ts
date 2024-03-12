510befe573902bd5b12213b5253af091e80875d3
aoyuru <102245971+1Aost@users.noreply.github.com>
2024-03-13 01:04:33 +0800

    build:github pages build

--- rspress.config.ts
+++ rspress.config.ts
@@ -5,11 +5,7 @@ export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: 'Aoblog-doc',
   description: 'Rspack-based Static Site Generator',
-  icon: '/rspress-icon.png',
-  logo: {
-    light: '/rspress-light-logo.png',
-    dark: '/rspress-dark-logo.png',
-  },
+  base: "/aoblog-fe-doc",
   themeConfig: {
     socialLinks: [
       { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
