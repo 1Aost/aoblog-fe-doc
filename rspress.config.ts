1ad6eddb4fe57a190d0fdd16eab4cb6dbb7e5666
aoyuru <102245971+1Aost@users.noreply.github.com>
2024-03-13 22:26:18 +0800

    Revert "feat:剩余组件的说明以及api生成表格插件的支持"
    This reverts commit 5e03a25a4ff23faa46222b8d45d7d1a559354b75.

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,7 +1,6 @@
 import * as path from 'path';
 import { defineConfig } from 'rspress/config';
 import { pluginPreview } from '@rspress/plugin-preview';
-import { pluginApiDocgen } from "@rspress/plugin-api-docgen";
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
@@ -15,13 +14,5 @@ export default defineConfig({
   },
   plugins: [
     pluginPreview(),
-    pluginApiDocgen({
-      entries: {
-        ActionRender: './src/api/Display/ActionRenderAPI.ts',
-        FilterSelect: './src/api/Header/FilterSelectAPI.ts',
-        HeaderGroup: './src/api/Header/HeaderGroupAPI.ts',
-      },
-      apiParseTool: 'react-docgen-typescript',
-    }),
   ]
 });
